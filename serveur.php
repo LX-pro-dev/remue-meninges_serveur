0b6024a3ee6edf0d1ae7d812627e204b8b0d7173
Alexandre GAUTHIER <lx.pro.dev@gmail.com>
2020-08-26 08:12:11 +0200

    creation post et get selon les regles API. pb ac json_encode

--- serveur.php
+++ serveur.php
@@ -1,48 +1,19 @@
 <?php
 include "functions.php";
+//print(json_encode($resultat)) bug à chaque requête !!!Undefined variable
 
-var_dump($_REQUEST);//affiche le contenu d'une variable ex :http://localhost/serveur.php?langue=fr&category=1 va afficher les infos correspondantes dans ma table. le ? correspond à la requête
+//var_dump($_REQUEST);//affiche le contenu d'une variable ex :http://localhost/serveur.php?langue=fr&category=1 va afficher les infos correspondantes dans ma table. le ? correspond à la requête
  
 //controle de réception de param
-if(isset($_REQUEST["operation"])){//operation REQUEST englobe ts les types de param, operation == param dans android à envoyer
-    
-    //enregistrement nouveau profil
-    if($_REQUEST["operation"]=="enreg"){
-         try{
-             //récupération des données en post
-             $lesdonnees =$REQUEST["lesdonnees"];//!mêmes noms à mettre dans android
-             //`id`, `langue`, `question`, `indice`, `reponse`, `category`, `level`, `dateceration`
-             $donnee= json_decode($lesdonnees);//décoder le json
-             $langue=$donnee[0];
-             $question=$donnee[1];
-             $indice=$donnee[2];
-             $reponse=$donnee[3];
-             $category=$donnee[4];
-             $level=$donnee[5];
-            
-                 
-             // insersion dans la bd
-             print("enreg%");
-             $cnx= connexionPDO();
-             $larequete = "insert into profil (langue,question,indice,reponse,category,level)";
-             $larequete.="values($langue,$question,$indice,$reponse,$categroy,$level)";
-             print ($larequete);
-             $req= $cnx->prepare($larequete);
-             $req->execute();
-             
-             
-        }catch(PDOException $e){
-            print "Erreur !%".$e->getMessage();
-            die();
-        }
-    }
-
+if(isset($_GET["operation"])){//operation REQUEST englobe ts les types de param, operation == param dans android à envoyer
+   
     //demande de récupération de tous les profils
-    elseif($_REQUEST["operation"]=="tous"){
+    if($_GET["operation"]=="tous"){//ou $_GET requete http (requete réseau)
+        $langue=$_GET["langue"];//récupérer la langue choisie
         try{
           print ("tous%");//% pour pouvoir faire un split sur le String de retour et donc le découper facilement
             $cnx = connexionPDO();//créer la connexion en récupérant la connexion de l'appel de la fonction connexionPDO()
-            $req = $cnx->prepare("select * from profil order by datecreation desc");//aller sur l'objet connexion et exécuter la méthode prepare qui attend une requete SQL et on veut récupérer dans l'ordre décroissant
+            $req = $cnx->prepare("select * from carte where langue = $langue order by datecreation desc");//aller sur l'objet connexion et exécuter la méthode prepare qui attend une requete SQL et on veut récupérer dans l'ordre décroissant
             $req->execute();//exécuter la requete
             
             //récupération de tous les profils
@@ -57,19 +28,108 @@ if(isset($_REQUEST["operation"])){//operation REQUEST englobe ts les types de pa
             die();
         }
     }
-
+    
+    elseif($_GET["operation"]=="category"){//ou $_GET requete http (requete réseau)
+        $langue=$_GET["langue"];
+        $category=$_GET["category"]; //récupérer la langue choisie et la category
+        try{
+          print ("category%");//% pour pouvoir faire un split sur le String de retour et donc le découper facilement
+            $cnx = connexionPDO();//créer la connexion en récupérant la connexion de l'appel de la fonction connexionPDO()
+            $req = $cnx->prepare("select * from carte where langue = $langue and categroy = $category order by datecreation desc");//aller sur l'objet connexion et exécuter la méthode prepare qui attend une requete SQL et on veut récupérer dans l'ordre décroissant
+            $req->execute();//exécuter la requete
+            
+            //récupération de tous les profils
+            //tant qu'il y a un profil, récupération du premier puisque c'est le dernier
+            while($ligne = $req ->fetch(PDO::FETCH_ASSOC)){//fetch lecture, fetch_assoc tableau associatif
+                //on récupère dans ligne la lecture que l'on met dans un tab. si la ligne est null il sort du while
+                $resultat[]=$ligne;
+            }
+            print(json_encode($resultat));//json_encode pour encoder le tableau en format json qui sera lu par android
+        }catch(PDOException $e){
+            print "Erreur !%".$e->getMessage();
+            die();
+        }
+    }
+    
+    elseif($_GET["operation"]=="level"){//ou $_GET requete http (requete réseau)
+        $langue=$_GET["langue"];
+        $level=$_GET["level"];//récupérer la langue et le level
+        try{
+          print ("level%");//% pour pouvoir faire un split sur le String de retour et donc le découper facilement
+            $cnx = connexionPDO();//créer la connexion en récupérant la connexion de l'appel de la fonction connexionPDO()
+            $req = $cnx->prepare("select * from carte order where langue = \"$langue\" and level = \"$level\" by datecreation desc");//aller sur l'objet connexion et exécuter la méthode prepare qui attend une requete SQL et on veut récupérer dans l'ordre décroissant
+            $req->execute();//exécuter la requete
+            
+            //récupération de tous les profils
+            //tant qu'il y a un profil, récupération du premier puisque c'est le dernier
+            while($ligne = $req ->fetch(PDO::FETCH_ASSOC)){//fetch lecture, fetch_assoc tableau associatif
+                //on récupère dans ligne la lecture que l'on met dans un tab. si la ligne est null il sort du while
+                $resultat[]=$ligne;
+            }
+            print(json_encode($resultat));//json_encode pour encoder le tableau en format json qui sera lu par android
+        }catch(PDOException $e){
+            print "Erreur !%".$e->getMessage();
+            die();
+        }
+    }
+    
+    elseif($_GET["operation"]=="date"){//ou $_GET requete http (requete réseau)
+        $langue=$_GET["langue"];
+        $date=$_GET["datecreation"];//récupérer la langue choisie et la date????
+        try{
+          print ("date%");//% pour pouvoir faire un split sur le String de retour et donc le découper facilement
+            $cnx = connexionPDO();//créer la connexion en récupérant la connexion de l'appel de la fonction connexionPDO()
+            $req = $cnx->prepare("SELECT * FROM carte WHERE langue = $langue and datecreation between \"$date\" and DATE_ADD(\"$date\",INTERVAL 1 DAY) order by datecreation desc");
+            $req->execute();//exécuter la requete
+            
+            //récupération de tous les profils
+            //tant qu'il y a un profil, récupération du premier puisque c'est le dernier
+            while($ligne = $req ->fetch(PDO::FETCH_ASSOC)){//fetch lecture, fetch_assoc tableau associatif
+                //on récupère dans ligne la lecture que l'on met dans un tab. si la ligne est null il sort du while
+                $resultat[]=$ligne;
+            }
+            print(json_encode($resultat));//json_encode pour encoder le tableau en format json qui sera lu par android
+        }catch(PDOException $e){
+            print "Erreur !%".$e->getMessage();
+            die();
+        }
+    }
+    
+    elseif($_GET["operation"]=="keyword"){//ou $_GET requete http (requete réseau)
+        $langue=$_GET["langue"];
+        $keyword=$_GET["keyword"];//récupérer la langue choisie et le mot clé
+        try{
+          print ("keyword%");//% pour pouvoir faire un split sur le String de retour et donc le découper facilement
+            $cnx = connexionPDO();//créer la connexion en récupérant la connexion de l'appel de la fonction connexionPDO()
+            $req = $cnx->prepare("select * from carte where langue = $langue and question like %$keyword% order by datecreation desc");//aller sur l'objet connexion et exécuter la méthode prepare qui attend une requete SQL et on veut récupérer dans l'ordre décroissant
+            $req->execute();//exécuter la requete
+            
+            //récupération de tous les profils
+            //tant qu'il y a un profil, récupération du premier puisque c'est le dernier
+            while($ligne = $req ->fetch(PDO::FETCH_ASSOC)){//fetch lecture, fetch_assoc tableau associatif
+                //on récupère dans ligne la lecture que l'on met dans un tab. si la ligne est null il sort du while
+                $resultat[]=$ligne;
+            }
+            print(json_encode($resultat));//json_encode pour encoder le tableau en format json qui sera lu par android
+        }catch(PDOException $e){
+            print "Erreur !%".$e->getMessage();
+            die();
+        }
+    }
+}
+elseif(isset($_DELETE["operation"])){//je ne suis pas certain d'être obligé de passer par cette instructio
     // demande de suppression
-    elseif($_REQUEST["operation"]=="del"){
+    if($_DELETE["operation"]=="del"){// ou $_DELETE ou $_GET["operation"]=="del"
          try{
              //récupération des données en post
-             $lesdonnees =$REQUEST["lesdonnees"];//!mêmes noms à mettre dans android
+             $lesdonnees =$_GET["lesdonnees"];//!mêmes noms à mettre dans android DELETE? GET?
              $donnee= json_decode($lesdonnees);//décoder le json
-             $id=$donnee[0];//j'ai remplacé $datemesure de serveurcoach par id car les 2 corresondaient à la PK !
+             $id=$donnee[0];//recuperation de la PK !
                           
              // suppression dans la bd
              print("del%");
              $cnx= connexionPDO();
-             $larequete = " delete from profil where id=$id";
+             $larequete = " delete from carte where id=$id";
              print ($larequete);
              $req= $cnx->prepare($larequete);
              $req->execute();
@@ -79,19 +139,60 @@ if(isset($_REQUEST["operation"])){//operation REQUEST englobe ts les types de pa
             die();
         }
     }
-    
-    // demande de modification
-    elseif($_REQUEST["operation"]=="update"){
+}
+elseif(isset($_POST["operation"])){//je ne suis pas certain d'être obligé de passer par cette instruction
+    //enregistrement nouveau profil
+    if($_POST["operation"]=="enreg"){
          try{
              //récupération des données en post
-             $lesdonnees =$REQUEST["lesdonnees"];//!mêmes noms à mettre dans android
+             $lesdonnees =$_GET["lesdonnees"];//!mêmes noms à mettre dans android !!!POST? GET?
+             var_dump($lesdonnees);
+
+             //`langue`, `question`, `indice`, `reponse`, `category`, `level`
              $donnee= json_decode($lesdonnees);//décoder le json
-             $id=$donnee[0];//j'ai remplacé $datemesure de serveurcoach par id car les 2 corresondaient à la PK !
-                          
+             $langue=$donnee[0];
+             $question=$donnee[1];
+             $indice=$donnee[2];
+             $reponse=$donnee[3];
+             $category=$donnee[4];
+             $level=$donnee[5];
+            
+                 
+             // insersion dans la bd
+             print("enreg%");
+             $cnx= connexionPDO();
+             $larequete = "insert into carte (langue,question,indice,reponse,category,level)";
+             $larequete.="values($langue,$question,$indice,$reponse,$categroy,$level)";
+             print ($larequete);
+             $req= $cnx->prepare($larequete);
+             $req->execute();
+             
+             
+        }catch(PDOException $e){
+            print "Erreur !%".$e->getMessage();
+            die();
+        }
+    }
+}
+if(isset($_PUT["operation"])){//je ne suis pas certain d'être obligé de passer par cette instructio
+    
+    // demande de modification
+    if($_PUT["operation"]=="update"){
+         try{
+             //récupération des données en post
+             $lesdonnees =$_GET["lesdonnees"];//format json de l'update! mêmes noms à mettre dans android PUT? GET?
+             $donnee= json_decode($lesdonnees);//décoder le json rq : le premier indice du tableau contiendra l'id
+             $id=$donnee[0];//recuperation de la PK !
+             $langue=$donnee[1];
+             $question=$donnee[2];
+             $indice=$donnee[3];
+             $reponse=$donnee[4];
+             $category=$donnee[5];
+             $level=$donnee[6];            
              // modification dans la bd
              print("update%");
              $cnx= connexionPDO();
-             $larequete = " update from profil where id=$id";
+             $larequete = "update from carte set langue = $langue, question = $question, indice = $indice, reponse = $reponse, category = $category, level = $level where id=$id";//on garde la date de creation sans sa maj?
              print ($larequete);
              $req= $cnx->prepare($larequete);
              $req->execute();
